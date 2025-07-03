159846e021c52b20eea9fd91deff2a88a8d0daa0
sharif57 <sharifmahamud577951@gmail.com>
2025-07-03 16:30:37 +0600

    cors add

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,19 @@ import express, { Application, Request, Response } from "express";
 import { model, Schema } from "mongoose";
 import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowBooks } from "./app/controllers/borrow.controller";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5000", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 app.use("/api", bookRoutes);
 app.use("/api", borrowBooks);
