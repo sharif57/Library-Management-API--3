2978bd0463a2a361bd09e4083f19a98fbeb08c42
sharif57 <sharifmahamud577951@gmail.com>
2025-07-03 16:33:35 +0600

    cors update

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5000", "http://localhost:3000"],
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
