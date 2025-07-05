f615f7a9e41e2d79e1a4250b233630686e24ce55
sharif57 <sharifmahamud577951@gmail.com>
2025-07-05 15:25:50 +0600

    cors add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import express, { Application, Request, Response } from "express";
-import { model, Schema } from "mongoose";
+import express, { Request, Response } from "express";
 import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowBooks } from "./app/controllers/borrow.controller";
 import cors from "cors";
@@ -9,18 +8,28 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://books-i2gx.vercel.app",
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "https://books-i2gx.vercel.app",
+        "http://localhost:3000", // For local development
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    optionsSuccessStatus: 204,
   })
 );
-
 
 app.use("/api", bookRoutes);
 app.use("/api", borrowBooks);
 
-app.get("/", async(req: Request, res: Response) => {
-  res.send("Hello")
-})
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello");
+});
 
 export default app;
